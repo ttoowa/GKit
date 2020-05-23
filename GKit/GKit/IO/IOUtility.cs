cdfa35f0110a5ce147c00a731f440d1da8424273
Bgoon <isg1153@naver.com>
2020-05-24 03:57:04 +0900

    -Path Normalize 및 비교 함수 추가

--- GKit/GKit/IO/IOUtility.cs
+++ GKit/GKit/IO/IOUtility.cs
@@ -178,5 +178,14 @@ namespace GKit {
 				}
 			}
 		}
+
+		public static string NormalizePath(string path) {
+			return Path.GetFullPath(new Uri(path).LocalPath)
+					   .TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
+					   .ToUpperInvariant();
+		}
+		public static bool ComparePath(string path1, string path2) {
+			return NormalizePath(path1) == NormalizePath(path2);
+		}
 	}
 }
