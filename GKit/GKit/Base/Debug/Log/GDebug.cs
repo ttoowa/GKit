0e5b6a97d0b9986c138c09e424d16c087070d513
AcrylicShrimp <led789zxpp@naver.com>
2020-05-31 21:33:46 +0900

    -빠진 파일 추가

---
+++ GKit/GKit/Base/Debug/Log/GDebug.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+#if OnUnity
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+#endif
+
+namespace GKit {
+	public static class GDebug {
+
+		[DllImport("kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+
+		private static bool onConsole;
+
+		static GDebug() {
+			onConsole = GetConsoleWindow() != IntPtr.Zero;
+		}
+		public static bool LogMode {
+			get; set;
+		} = true;
+		public static void Log(object text, GLogLevel logLevel = 0) {
+			if (!LogMode)
+				return;
+			switch (logLevel) {
+				default:
+				case GLogLevel.Log:
+					LogPlatform($"GDebug_Log :: {text}");
+					break;
+				case GLogLevel.Warnning:
+					LogPlatform($"GDebug_Warning :: {text}");
+					break;
+				case GLogLevel.Error:
+					LogPlatform($"GDebug_Error :: {text}");
+					throw new Exception($"GLog_Error :: {text}");
+			}
+		}
+		private static void LogPlatform(string text) {
+#if OnUnity
+			Debug.Log(
+#else
+			if(onConsole) {
+				Console.WriteLine(text);
+			}
+			Debug.WriteLine(
+#endif
+						text);
+		}
+	}
+}
