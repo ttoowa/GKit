c6d1824bff5e3fc2b6231cbfcc255e192f8f8cb4
Bgoon <isg1153@naver.com>
2020-07-12 19:45:16 +0900

    -실수로 작성한 MouseInput ScrollChanged 변수를 이벤트로 변경

--- GKit/GKit/Base/Input/MouseInput/MouseInput.cs
+++ GKit/GKit/Base/Input/MouseInput/MouseInput.cs
@@ -66,7 +66,7 @@ namespace GKit
 #endif
 
 #if OnUnity
-		public static ScrollChangedDelegate ScrollChanged;
+		public static event ScrollChangedDelegate ScrollChanged;
 #endif
 
 		static MouseInput() {
