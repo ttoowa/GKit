f05d0b2f963f13feefad87d9db5984b7786a1ea7
Bgoon <isg1153@naver.com>
2020-10-25 17:11:43 +0900

    -CollisionHandler 추가

---
+++ GKit/GKitForUnity/Unity/EventHandler/CollisionHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GKitForUnity.Unity.EventHandler {
+	public delegate void CollisionEventDelegate(Collision collision);
+	public delegate void TriggerEventDelegate(Collider collider);
+
+	public class CollisionHandler : MonoBehaviour {
+		public event CollisionEventDelegate ColliderEnter;
+		public event CollisionEventDelegate ColliderStay;
+		public event CollisionEventDelegate ColliderExit;
+
+		public event TriggerEventDelegate TriggerEnter;
+		public event TriggerEventDelegate TriggerStay;
+		public event TriggerEventDelegate TriggerExit;
+
+		private void OnCollisionEnter(Collision collision) {
+			ColliderEnter?.Invoke(collision);
+		}
+		private void OnCollisionStay(Collision collision) {
+			ColliderStay?.Invoke(collision);
+		}
+		private void OnCollisionExit(Collision collision) {
+			ColliderExit?.Invoke(collision);
+		}
+
+
+		private void OnTriggerEnter(Collider collider) {
+			TriggerEnter?.Invoke(collider);
+		}
+		private void OnTriggerStay(Collider collider) {
+			TriggerStay?.Invoke(collider);
+		}
+		private void OnTriggerExit(Collider collider) {
+			TriggerExit?.Invoke(collider);
+		}
+	}
+}
---
+++ GKit/GKitForUnity/Unity/EventHandler/CollisionHandler2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GKitForUnity.Unity.EventHandler {
+	public delegate void Collision2DEventDelegate(Collision2D collision);
+	public delegate void Trigger2DEventDelegate(Collider2D collider);
+
+	public class CollisionHandler2D : MonoBehaviour {
+		public event Collision2DEventDelegate ColliderEnter2D;
+		public event Collision2DEventDelegate ColliderStay2D;
+		public event Collision2DEventDelegate ColliderExit2D;
+
+		public event Trigger2DEventDelegate TriggerEnter2D;
+		public event Trigger2DEventDelegate TriggerStay2D;
+		public event Trigger2DEventDelegate TriggerExit2D;
+
+		private void OnCollisionEnter2D(Collision2D collision) {
+			ColliderEnter2D?.Invoke(collision);
+		}
+		private void OnCollisionStay2D(Collision2D collision) {
+			ColliderStay2D?.Invoke(collision);
+		}
+		private void OnCollisionExit2D(Collision2D collision) {
+			ColliderExit2D?.Invoke(collision);
+		}
+
+
+		private void OnTriggerEnter2D(Collider2D collider) {
+			TriggerEnter2D?.Invoke(collider);
+		}
+		private void OnTriggerStay2D(Collider2D collider) {
+			TriggerStay2D?.Invoke(collider);
+		}
+		private void OnTriggerExit2D(Collider2D collider) {
+			TriggerExit2D?.Invoke(collider);
+		}
+	}
+}
