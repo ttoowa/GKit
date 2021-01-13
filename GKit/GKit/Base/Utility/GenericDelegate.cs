77ceaaff0eeca9bbde3cf6be910b152992c89216
Bgoon <isg1153@naver.com>
2021-01-13 21:35:21 +0900

    -ValueChangedDelegate 인수 순서변경

--- GKit/GKit/Base/Utility/GenericDelegate.cs
+++ GKit/GKit/Base/Utility/GenericDelegate.cs
@@ -21,5 +21,5 @@ namespace GKit
 	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2>(ArgT1 value1, ArgT2 value2);
 	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3>(ArgT1 value1, ArgT2 value2, ArgT3 value3);
 	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3, ArgT4>(ArgT1 value1, ArgT2 value2, ArgT3 value3, ArgT4 value4);
-	public delegate void ValueChangedDelegate<T>(T oldValue, T newValue);
+	public delegate void ValueChangedDelegate<T>(T newValue, T oldValue);
 }
