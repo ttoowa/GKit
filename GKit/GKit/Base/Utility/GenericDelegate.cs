21af549374299f7d66d089a599e6c3809e6a7cf9
Bgoon <isg1153@naver.com>
2021-01-13 21:06:34 +0900

    -GenericDelegate 파일로 분리

---
+++ GKit/GKit/Base/Utility/GenericDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if OnUnity
+namespace GKitForUnity
+#elif OnWPF
+namespace GKitForWPF
+#else
+namespace GKit
+#endif
+{
+	public delegate void Arg1Delegate<ArgT>(ArgT value);
+	public delegate void Arg2Delegate<ArgT1, ArgT2>(ArgT1 value1, ArgT2 value2);
+	public delegate void Arg3Delegate<ArgT1, ArgT2, ArgT3>(ArgT1 value1, ArgT2 value2, ArgT3 value3);
+	public delegate void Arg3Delegate<ArgT1, ArgT2, ArgT3, ArgT4>(ArgT1 value1, ArgT2 value2, ArgT3 value3, ArgT4 value4);
+	public delegate ReturnT ReturnDelegate<ReturnT>();
+	public delegate ReturnT Arg1ReturnDelegate<ReturnT, ArgT>(ArgT value);
+	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2>(ArgT1 value1, ArgT2 value2);
+	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3>(ArgT1 value1, ArgT2 value2, ArgT3 value3);
+	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3, ArgT4>(ArgT1 value1, ArgT2 value2, ArgT3 value3, ArgT4 value4);
+	public delegate void ValueChangedDelegate<T>(T oldValue, T newValue);
+}
--- GKit/GKit/Base/Utility/SystemUtility.cs
+++ GKit/GKit/Base/Utility/SystemUtility.cs
@@ -16,16 +16,6 @@ namespace GKitForWPF
 namespace GKit
 #endif
 {
-	public delegate void Arg1Delegate<ArgT>(ArgT value);
-	public delegate void Arg2Delegate<ArgT1, ArgT2>(ArgT1 value1, ArgT2 value2);
-	public delegate void Arg3Delegate<ArgT1, ArgT2, ArgT3>(ArgT1 value1, ArgT2 value2, ArgT3 value3);
-	public delegate void Arg3Delegate<ArgT1, ArgT2, ArgT3, ArgT4>(ArgT1 value1, ArgT2 value2, ArgT3 value3, ArgT4 value4);
-	public delegate ReturnT ReturnDelegate<ReturnT>();
-	public delegate ReturnT Arg1ReturnDelegate<ReturnT, ArgT>(ArgT value);
-	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2>(ArgT1 value1, ArgT2 value2);
-	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3>(ArgT1 value1, ArgT2 value2, ArgT3 value3);
-	public delegate ReturnT Arg2ReturnDelegate<ReturnT, ArgT1, ArgT2, ArgT3, ArgT4>(ArgT1 value1, ArgT2 value2, ArgT3 value3, ArgT4 value4);
-
 	public static class SystemExtension {
 		public static float GetElapsedMilliseconds(this Stopwatch stopwatch) {
 			return stopwatch.ElapsedTicks / (float)Stopwatch.Frequency * 1000f;
