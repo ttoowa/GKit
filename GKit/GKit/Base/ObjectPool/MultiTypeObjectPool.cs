140edef06307a6ba64732531b7081b776fe35487
TTOOWA <isg1153@naver.com>
2022-06-15 03:13:11 +0900

    -ObjectPool 을 생성할 때 Instance를 만들지 않도록 수정

--- GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
+++ GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
@@ -12,61 +12,60 @@ namespace GKitForWPF
 namespace GKit
 #endif
 {
-    public class MultiTypeObjectPool {
-        public delegate IPoolable CreateInstanceDelegate(Type type);
+	public class MultiTypeObjectPool {
+		public delegate IPoolable CreateInstanceDelegate(Type type);
 
-        private readonly Dictionary<Type, ObjectPool<IPoolable>> poolDict;
+		private readonly Dictionary<Type, ObjectPool<IPoolable>> poolDict;
 
-        private readonly CreateInstanceDelegate CreateInstanceMethod;
-        private readonly Arg1Delegate<IPoolable> DisposeInstanceMethod;
-        private readonly Arg1Delegate<IPoolable> GetInstanceMethod;
-        private readonly Arg1Delegate<IPoolable> ReturnInstanceMethod;
+		private readonly CreateInstanceDelegate CreateInstanceMethod;
+		private readonly Arg1Delegate<IPoolable> DisposeInstanceMethod;
+		private readonly Arg1Delegate<IPoolable> GetInstanceMethod;
+		private readonly Arg1Delegate<IPoolable> ReturnInstanceMethod;
 
 
-        public MultiTypeObjectPool(CreateInstanceDelegate createInstanceMethod = null, Arg1Delegate<IPoolable> disposeInstanceMethod = null,
-         Arg1Delegate<IPoolable> getInstanceMethod = null, Arg1Delegate<IPoolable> returnInstanceMethod = null) {
-            poolDict = new Dictionary<Type, ObjectPool<IPoolable>>();
+		public MultiTypeObjectPool(CreateInstanceDelegate createInstanceMethod = null, Arg1Delegate<IPoolable> disposeInstanceMethod = null,
+		 Arg1Delegate<IPoolable> getInstanceMethod = null, Arg1Delegate<IPoolable> returnInstanceMethod = null) {
+			poolDict = new Dictionary<Type, ObjectPool<IPoolable>>();
 
-            this.CreateInstanceMethod = createInstanceMethod;
-            this.DisposeInstanceMethod = disposeInstanceMethod;
-            this.GetInstanceMethod = getInstanceMethod;
-            this.ReturnInstanceMethod = returnInstanceMethod;
-        }
+			this.CreateInstanceMethod = createInstanceMethod;
+			this.DisposeInstanceMethod = disposeInstanceMethod;
+			this.GetInstanceMethod = getInstanceMethod;
+			this.ReturnInstanceMethod = returnInstanceMethod;
+		}
 
-        public IPoolable GetInstance(Type type) {
-            ObjectPool<IPoolable> pool = GetOrCreatePool(type);
+		public IPoolable GetInstance(Type type) {
+			ObjectPool<IPoolable> pool = GetOrCreatePool(type);
 
-            return pool.GetInstance();
-        }
-        public void ReturnInstance(IPoolable instance) {
-            Type type = instance.GetType();
-            ObjectPool<IPoolable> pool = GetOrCreatePool(type);
-            pool.ReturnInstance(instance);
-        }
+			return pool.GetInstance();
+		}
+		public void ReturnInstance(IPoolable instance) {
+			Type type = instance.GetType();
+			ObjectPool<IPoolable> pool = GetOrCreatePool(type);
+			pool.ReturnInstance(instance);
+		}
 
-        private ObjectPool<IPoolable> GetOrCreatePool(Type type) {
-            ObjectPool<IPoolable> pool;
-            if (poolDict.ContainsKey(type)) {
-                pool = poolDict[type];
-            } else {
-                pool = new ObjectPool<IPoolable>();
-                poolDict.Add(type, pool);
+		private ObjectPool<IPoolable> GetOrCreatePool(Type type) {
+			ObjectPool<IPoolable> pool;
+			if (poolDict.ContainsKey(type)) {
+				pool = poolDict[type];
+			} else {
+				pool = new ObjectPool<IPoolable>(() => {
+					return CreateInstanceMethod(type);
+				});
+				poolDict.Add(type, pool);
 
-                pool.CreateInstanceMethod = () => {
-                    return CreateInstanceMethod(type);
-                };
-                pool.DisposeInstanceMethod += (IPoolable instance) => {
-                    DisposeInstanceMethod?.Invoke(instance);
-                };
-                pool.GetInstanceMethod += (IPoolable instance) => {
-                    GetInstanceMethod?.Invoke(instance);
-                };
-                pool.ReturnInstanceMethod += (IPoolable instance) => {
-                    ReturnInstanceMethod?.Invoke(instance);
-                };
-            }
+				pool.DisposeInstanceMethod += (IPoolable instance) => {
+					DisposeInstanceMethod?.Invoke(instance);
+				};
+				pool.GetInstanceMethod += (IPoolable instance) => {
+					GetInstanceMethod?.Invoke(instance);
+				};
+				pool.ReturnInstanceMethod += (IPoolable instance) => {
+					ReturnInstanceMethod?.Invoke(instance);
+				};
+			}
 
-            return pool;
-        }
-    }
+			return pool;
+		}
+	}
 }
