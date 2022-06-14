8b49364d444f6b41c0b9525801a87b378b668866
TTOOWA <isg1153@naver.com>
2022-06-15 00:55:13 +0900

    -GKit Windows 종속성 복구

--- GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
+++ GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
@@ -13,38 +13,38 @@ namespace GKit
 #endif
 {
     public class MultiTypeObjectPool {
+        public delegate IPoolable CreateInstanceDelegate(Type type);
 
         private readonly Dictionary<Type, ObjectPool<IPoolable>> poolDict;
 
-        public MultiTypeObjectPool() {
+        private readonly CreateInstanceDelegate CreateInstanceMethod;
+        private readonly Arg1Delegate<IPoolable> DisposeInstanceMethod;
+        private readonly Arg1Delegate<IPoolable> GetInstanceMethod;
+        private readonly Arg1Delegate<IPoolable> ReturnInstanceMethod;
+
+
+        public MultiTypeObjectPool(CreateInstanceDelegate createInstanceMethod = null, Arg1Delegate<IPoolable> disposeInstanceMethod = null,
+         Arg1Delegate<IPoolable> getInstanceMethod = null, Arg1Delegate<IPoolable> returnInstanceMethod = null) {
             poolDict = new Dictionary<Type, ObjectPool<IPoolable>>();
+
+            this.CreateInstanceMethod = createInstanceMethod;
+            this.DisposeInstanceMethod = disposeInstanceMethod;
+            this.GetInstanceMethod = getInstanceMethod;
+            this.ReturnInstanceMethod = returnInstanceMethod;
         }
 
         public IPoolable GetInstance(Type type) {
-            ObjectPool<IPoolable> pool;
-            if (poolDict.ContainsKey(type)) {
-                pool = poolDict[type];
-            } else {
-                pool = new ObjectPool<IPoolable>();
-                poolDict.Add(type, pool);
-            }
+            ObjectPool<IPoolable> pool = GetOrCreatePool(type);
 
             return pool.GetInstance();
         }
         public void ReturnInstance(IPoolable instance) {
             Type type = instance.GetType();
-            ObjectPool<IPoolable> pool;
-            if (poolDict.ContainsKey(type)) {
-                pool = poolDict[type];
-            } else {
-                pool = new ObjectPool<IPoolable>();
-                poolDict.Add(type, pool);
-            }
-
+            ObjectPool<IPoolable> pool = GetOrCreatePool(type);
             pool.ReturnInstance(instance);
         }
 
-        private ObjectPool<IPoolable> GetOrCreatePool<T>() {
+        private ObjectPool<IPoolable> GetOrCreatePool(Type type) {
             ObjectPool<IPoolable> pool;
             if (poolDict.ContainsKey(type)) {
                 pool = poolDict[type];
@@ -52,8 +52,15 @@ namespace GKit
                 pool = new ObjectPool<IPoolable>();
                 poolDict.Add(type, pool);
 
-                pool.CreateInstanceMethod = () => {
-
+                pool.CreateInstanceMethod = CreateInstanceMethod;
+                pool.DisposeInstanceMethod += (IPoolable instance) => {
+                    DisposeInstanceMethod?.Invoke(instance);
+                };
+                pool.GetInstanceMethod += (IPoolable instance) => {
+                    GetInstanceMethod?.Invoke(instance);
+                };
+                pool.ReturnInstanceMethod += (IPoolable instance) => {
+                    ReturnInstanceMethod?.Invoke(instance);
                 };
             }
 
