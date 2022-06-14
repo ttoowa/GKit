7d4451487cbbe99cf12b3de96c1668f46f7f263a
-g <-g>
2022-06-14 22:52:15 +0900

    -MultiObjectPool 작업 중

--- GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
+++ GKit/GKit/Base/ObjectPool/MultiTypeObjectPool.cs
@@ -1,89 +1,63 @@
-﻿// using System;
-// using System.Collections.Generic;
-// #if OnUnity
-// using UnityEngine;
-// #endif
+﻿using System;
+using System.Collections.Generic;
+#if OnUnity
+using UnityEngine;
+#endif
 
-// #if OnUnity
-// namespace GKitForUnity
-// #elif OnWPF
-// namespace GKitForWPF
-// #else
-// namespace GKit
-// #endif
-// {
+#if OnUnity
+namespace GKitForUnity
+#elif OnWPF
+namespace GKitForWPF
+#else
+namespace GKit
+#endif
+{
+    public class MultiTypeObjectPool {
 
-//     public class MultiTypeObjectPool {
-//         //public int Count => targetCount;
+        private readonly Dictionary<Type, ObjectPool<IPoolable>> poolDict;
 
-//         //public bool IsFull => objectStack.Count == targetCount;
-//         //private int targetCount;
-//         //private Stack<T> objectStack;
-//         //public ReturnDelegate<T> CreateInstanceMethod;
-//         //public event Arg1Delegate<T> DisposeInstanceTask;
-//         //public event Arg1Delegate<T> GetInstanceTask;
-//         //public event Arg1Delegate<T> ReturnInstanceTask;
+        public MultiTypeObjectPool() {
+            poolDict = new Dictionary<Type, ObjectPool<IPoolable>>();
+        }
 
-//         private Dictionary<Type, ObjectPool<IPoolable>> poolDict = new Dictionary<Type, ObjectPool<IPoolable>>();
+        public IPoolable GetInstance(Type type) {
+            ObjectPool<IPoolable> pool;
+            if (poolDict.ContainsKey(type)) {
+                pool = poolDict[type];
+            } else {
+                pool = new ObjectPool<IPoolable>();
+                poolDict.Add(type, pool);
+            }
 
-//         public MultiTypeObjectPool() {
-//         }
+            return pool.GetInstance();
+        }
+        public void ReturnInstance(IPoolable instance) {
+            Type type = instance.GetType();
+            ObjectPool<IPoolable> pool;
+            if (poolDict.ContainsKey(type)) {
+                pool = poolDict[type];
+            } else {
+                pool = new ObjectPool<IPoolable>();
+                poolDict.Add(type, pool);
+            }
 
-//         public IPoolable GetInstance(Type type) {
-//             ObjectPool<IPoolable> pool;
-//             if (poolDict.ContainsKey(type)) {
-//                 pool = poolDict[type];
-//             } else {
-//                 pool = new ObjectPool<IPoolable>();
-//                 poolDict.Add(type, pool);
-//             }
+            pool.ReturnInstance(instance);
+        }
 
-//             return pool.GetInstance();
+        private ObjectPool<IPoolable> GetOrCreatePool<T>() {
+            ObjectPool<IPoolable> pool;
+            if (poolDict.ContainsKey(type)) {
+                pool = poolDict[type];
+            } else {
+                pool = new ObjectPool<IPoolable>();
+                poolDict.Add(type, pool);
 
-//             if (objectStack.Count == 0) {
-//                 CreateInstance(1);
-//             }
-//             T instance = objectStack.Pop();
-//             GetInstanceTask?.Invoke(instance);
-//             return instance;
-//         }
-//         public void ReturnInstance(T instance) {
-//             ReturnInstanceTask?.Invoke(instance);
-//             if (objectStack.Count < targetCount) {
-//                 objectStack.Push(instance);
-//             } else {
-//                 DisposeInstanceTask?.Invoke(instance);
-//             }
-//         }
+                pool.CreateInstanceMethod = () => {
 
-//         public void CreateInstance(int count) {
-//             if (count == 0)
-//                 return;
+                };
+            }
 
-//             this.targetCount += count;
-//             for (int i = 0; i < count; ++i) {
-//                 T instance;
-//                 if (CreateInstanceMethod != null) {
-//                     instance = CreateInstanceMethod();
-//                 } else {
-//                     instance = default(T);
-//                 }
-//                 ReturnInstanceTask?.Invoke(instance);
-//                 objectStack.Push(instance);
-//             }
-//         }
-//         public void DeleteInstance(int deleteCount) {
-//             deleteCount = Mathf.Min(deleteCount, targetCount);
-//             targetCount = Mathf.Max(targetCount - deleteCount, 0);
-
-//             int immediateDeleteCount = Mathf.Min(deleteCount, objectStack.Count);
-//             for (int i = 0; i < immediateDeleteCount; ++i) {
-//                 OnDisposeInstance(objectStack.Pop());
-//             }
-//         }
-
-//         private void OnDisposeInstance(T obj) {
-//             DisposeInstanceTask?.Invoke(obj);
-//         }
-//     }
-// }
+            return pool;
+        }
+    }
+}
