3ecdc9609fcef6d9baa75690af61f54341fc54f4
TTOOWA <isg1153@naver.com>
2022-06-25 03:04:28 +0900

    -UnityEngine DLL참조 모듈형으로 교체

--- GKit/GKitForUnity/Unity/Utility/AssetUtility.cs
+++ GKit/GKitForUnity/Unity/Utility/AssetUtility.cs
@@ -2,22 +2,21 @@
 using UnityEngine;
 
 namespace GKitForUnity.Unity.Utility {
-	public static class AssetUtility {
-		//Resource
-		public static T GetResource<T>(this string path) where T : UnityEngine.Object {
-			return GResourceUtility.Get<T>(path);
-		}
-		public static Texture2D GetTexture(this string localPath, TextureFormat format = TextureFormat.RGBA32, bool generateMipmap = true) {
-			if (File.Exists(localPath)) {
-				byte[] binary = File.ReadAllBytes(localPath);
-				Texture2D tex = new Texture2D(2, 2, format, generateMipmap);
-				tex.LoadImage(binary);
-				return tex;
-			}
-			return null;
-		}
+    public static class AssetUtility {
+        //Resource
+        public static T GetResource<T>(this string path) where T : UnityEngine.Object {
+            return GResourceUtility.Get<T>(path);
+        }
 
+        public static Texture2D GetTexture(this string localPath, TextureFormat format = TextureFormat.RGBA32, bool generateMipmap = true) {
+            if (File.Exists(localPath)) {
+                byte[] binary = File.ReadAllBytes(localPath);
+                Texture2D tex = new Texture2D(2, 2, format, generateMipmap);
+                tex.LoadRawTextureData(binary);
+                return tex;
+            }
 
-
-	}
+            return null;
+        }
+    }
 }
