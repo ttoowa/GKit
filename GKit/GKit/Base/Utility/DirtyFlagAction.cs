2c97e9de90524cc23d1d1f438ad169db750ac34b
TTOOWA <isg1153@naver.com>
2022-09-08 07:58:00 +0900

    - DirtyFlagAction namespace 지정

--- GKit/GKit/Base/Utility/DirtyFlagAction.cs
+++ GKit/GKit/Base/Utility/DirtyFlagAction.cs
@@ -1,28 +1,33 @@
-using System;
+#if OnUnity
+namespace GKitForUnity
+#elif OnWPF
+namespace GKitForWPF
+#else
+namespace GKit
+#endif
+{
+    public class DirtyFlagAction {
+        public bool IsDirty { get; private set; }
 
-namespace GKit;
+        private readonly Action action;
 
-public class DirtyFlagAction {
-    public bool IsDirty { get; private set; }
-
-    private readonly Action action;
-
-    public DirtyFlagAction(Action action) {
-        this.action = action;
-    }
-
-    public void SetDirty() {
-        IsDirty = true;
-    }
-
-    public bool Invoke(bool force = false) {
-        if (IsDirty || force) {
-            IsDirty = false;
-
-            action?.Invoke();
-            return true;
+        public DirtyFlagAction(Action action) {
+            this.action = action;
         }
 
-        return false;
+        public void SetDirty() {
+            IsDirty = true;
+        }
+
+        public bool Invoke(bool force = false) {
+            if (IsDirty || force) {
+                IsDirty = false;
+
+                action?.Invoke();
+                return true;
+            }
+
+            return false;
+        }
     }
 }
