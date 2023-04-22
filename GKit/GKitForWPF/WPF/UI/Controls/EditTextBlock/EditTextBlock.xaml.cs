653d0e71094fe4afb580fdab2dbc5c88c6f7f373
TTOOWA <isg1153@naver.com>
2023-04-23 00:20:32 +0900

    - EditTextBlock에서 한글 입력 후 EndEdit 시 마지막 글자가 잘리는 현상 Fix

--- GKit/GKitForWPF/WPF/UI/Controls/EditTextBlock/EditTextBlock.xaml.cs
+++ GKit/GKitForWPF/WPF/UI/Controls/EditTextBlock/EditTextBlock.xaml.cs
@@ -6,139 +6,113 @@ using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace GKitForWPF.UI.Controls {
-	public delegate void TextEditedDelegate(string oldText, string newText, ref bool cancelEdit);
+namespace GKitForWPF.UI.Controls; 
 
-	public partial class EditTextBlock : UserControl {
-		public static readonly DependencyProperty IsEditingProperty = DependencyProperty.RegisterAttached(nameof(IsEditing), typeof(bool), typeof(EditTextBlock), new PropertyMetadata(false));
-		public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(nameof(Text), typeof(string), typeof(EditTextBlock), new PropertyMetadata(null));
-		public static readonly DependencyProperty EditingTextProperty = DependencyProperty.RegisterAttached(nameof(EditingText), typeof(string), typeof(EditTextBlock), new PropertyMetadata(null));
-		public static readonly DependencyProperty EditingBackgroundProperty = DependencyProperty.RegisterAttached(nameof(EditingBackground), typeof(Brush), typeof(EditTextBlock), new PropertyMetadata("FFFFFF".ToBrush()));
-		public static readonly DependencyProperty EditingForegroundProperty = DependencyProperty.RegisterAttached(nameof(EditingForeground), typeof(Brush), typeof(EditTextBlock), new PropertyMetadata("666666".ToBrush()));
+public delegate void TextEditedDelegate(string oldText, string newText, ref bool cancelEdit);
 
-		public event TextEditedDelegate TextEdited;
+public partial class EditTextBlock : UserControl {
+    public static readonly DependencyProperty IsEditingProperty =
+        DependencyProperty.RegisterAttached(nameof(IsEditing), typeof(bool), typeof(EditTextBlock), new PropertyMetadata(false));
 
-		public bool IsEditing {
-			get {
-				return (bool)GetValue(IsEditingProperty);
-			}
-			set {
-				SetValue(IsEditingProperty, value);
-			}
-		}
+    public static readonly DependencyProperty TextProperty = DependencyProperty.RegisterAttached(nameof(Text), typeof(string), typeof(EditTextBlock), new PropertyMetadata(null));
 
-		public string Text {
-			get {
-				return (string)GetValue(TextProperty);
-			}
-			set {
-				SetValue(TextProperty, value);
-			}
-		}
-		public string EditingText {
-			get {
-				return (string)GetValue(EditingTextProperty);
-			}
-			set {
-				SetValue(EditingTextProperty, value);
-			}
-		}
+    public static readonly DependencyProperty EditingBackgroundProperty =
+        DependencyProperty.RegisterAttached(nameof(EditingBackground), typeof(Brush), typeof(EditTextBlock), new PropertyMetadata("FFFFFF".ToBrush()));
 
-		public Brush EditingBackground {
-			get {
-				return (Brush)GetValue(EditingBackgroundProperty);
-			}
-			set {
-				SetValue(EditingBackgroundProperty, value);
-			}
-		}
-		public Brush EditingForeground {
-			get {
-				return (Brush)GetValue(EditingBackgroundProperty);
-			}
-			set {
-				SetValue(EditingBackgroundProperty, value);
-			}
-		}
+    public static readonly DependencyProperty EditingForegroundProperty =
+        DependencyProperty.RegisterAttached(nameof(EditingForeground), typeof(Brush), typeof(EditTextBlock), new PropertyMetadata("666666".ToBrush()));
 
-		public EditTextBlock() {
-			InitializeComponent();
-			InitBindings();
-		}
-		private void InitBindings() {
-			StaticTextBlock.SetBinding(TextBlock.FontSizeProperty, new Binding(nameof(FontSize)) { Source = this, Mode = BindingMode.OneWay });
-			StaticTextBlock.SetBinding(TextBlock.FontFamilyProperty, new Binding(nameof(FontFamily)) { Source = this, Mode = BindingMode.OneWay });
-			StaticTextBlock.SetBinding(TextBlock.TextProperty, new Binding(nameof(Text)) { Source = this, Mode = BindingMode.OneWay });
-			StaticTextBlock.SetBinding(TextBlock.ForegroundProperty, new Binding(nameof(Foreground)) { Source = this, Mode = BindingMode.OneWay });
+    public event TextEditedDelegate TextEdited;
 
-			EditingTextBox.SetBinding(TextBox.FontSizeProperty, new Binding(nameof(FontSize)) { Source = this, Mode = BindingMode.OneWay });
-			EditingTextBox.SetBinding(TextBox.FontFamilyProperty, new Binding(nameof(FontFamily)) { Source = this, Mode = BindingMode.OneWay });
-			EditingTextBox.SetBinding(TextBox.TextProperty, new Binding(nameof(EditingText)) { Source = this, Mode = BindingMode.TwoWay, UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged });
-			EditingTextBox.SetBinding(TextBox.BackgroundProperty, new Binding(nameof(EditingBackground)) { Source = this, Mode = BindingMode.OneWay });
-			EditingTextBox.SetBinding(TextBox.ForegroundProperty, new Binding(nameof(EditingForeground)) { Source = this, Mode = BindingMode.OneWay });
-			EditingTextBox.SetBinding(TextBox.VisibilityProperty, new Binding(nameof(IsEditing)) { Source = this, Mode = BindingMode.OneWay, Converter = new BoolToVisibilityConverter() });
+    public bool IsEditing {
+        get => (bool)GetValue(IsEditingProperty);
+        set => SetValue(IsEditingProperty, value);
+    }
 
-			EventArea.SetBinding(Border.IsHitTestVisibleProperty, new Binding(nameof(IsEditing)) { Source = this, Mode = BindingMode.OneWay, Converter = new BoolInvertConverter() });
-		}
+    public string Text {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
 
-		public void StartEditing() {
-			IsEditing = true;
-			StartCaptureMouse();
+    public Brush EditingBackground {
+        get => (Brush)GetValue(EditingBackgroundProperty);
+        set => SetValue(EditingBackgroundProperty, value);
+    }
+
+    public Brush EditingForeground {
+        get => (Brush)GetValue(EditingBackgroundProperty);
+        set => SetValue(EditingBackgroundProperty, value);
+    }
+
+    public EditTextBlock() {
+        InitializeComponent();
+        InitBindings();
+    }
+
+    private void InitBindings() {
+        StaticTextBlock.SetBinding(TextBlock.FontSizeProperty, new Binding(nameof(FontSize)) { Source = this, Mode = BindingMode.OneWay });
+        StaticTextBlock.SetBinding(TextBlock.FontFamilyProperty, new Binding(nameof(FontFamily)) { Source = this, Mode = BindingMode.OneWay });
+        StaticTextBlock.SetBinding(TextBlock.TextProperty, new Binding(nameof(Text)) { Source = this, Mode = BindingMode.OneWay });
+        StaticTextBlock.SetBinding(TextBlock.ForegroundProperty, new Binding(nameof(Foreground)) { Source = this, Mode = BindingMode.OneWay });
+
+        EditingTextBox.SetBinding(FontSizeProperty, new Binding(nameof(FontSize)) { Source = this, Mode = BindingMode.OneWay });
+        EditingTextBox.SetBinding(FontFamilyProperty, new Binding(nameof(FontFamily)) { Source = this, Mode = BindingMode.OneWay });
+        EditingTextBox.SetBinding(BackgroundProperty, new Binding(nameof(EditingBackground)) { Source = this, Mode = BindingMode.OneWay });
+        EditingTextBox.SetBinding(ForegroundProperty, new Binding(nameof(EditingForeground)) { Source = this, Mode = BindingMode.OneWay });
+        EditingTextBox.SetBinding(VisibilityProperty, new Binding(nameof(IsEditing)) { Source = this, Mode = BindingMode.OneWay, Converter = new BoolToVisibilityConverter() });
+
+        EventArea.SetBinding(IsHitTestVisibleProperty, new Binding(nameof(IsEditing)) { Source = this, Mode = BindingMode.OneWay, Converter = new BoolInvertConverter() });
+    }
+
+    public void StartEditing() {
+        IsEditing = true;
+        StartCaptureMouse();
 
 
-			EditingText = Text;
-			EditingTextBox.Focus();
-			EditingTextBox.CaretIndex = EditingTextBox.Text.Length;
-		}
-		public void EndEditing() {
-			IsEditing = false;
-			StopCaptureMouse();
+        EditingTextBox.Text = Text;
+        EditingTextBox.Focus();
+        EditingTextBox.CaretIndex = EditingTextBox.Text.Length;
+    }
 
-			if (Text == EditingText)
-				return;
+    public void EndEditing() {
+        IsEditing = false;
+        StopCaptureMouse();
 
-			bool cancelEdit = false;
-			TextEdited?.Invoke(Text, EditingText, ref cancelEdit);
+        if (Text == EditingTextBox.Text)
+            return;
 
-			if (cancelEdit)
-				return;
+        bool cancelEdit = false;
+        TextEdited?.Invoke(Text, EditingTextBox.Text, ref cancelEdit);
 
-			Text = EditingText;
-		}
+        if (cancelEdit)
+            return;
 
-		private void StartCaptureMouse() {
-			InputManager.Current.PreProcessInput += InputManager_PreProcessInput;
-		}
-		private void StopCaptureMouse() {
-			InputManager.Current.PreProcessInput -= InputManager_PreProcessInput;
-		}
+        Text = EditingTextBox.Text;
+    }
 
-		private void EventArea_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
-			if (e.ChangedButton == MouseButton.Left) {
-				StartEditing();
-				e.Handled = true;
-			}
-		}
-		private void InputManager_PreProcessInput(object sender, PreProcessInputEventArgs e) {
-			if (e.StagingItem.Input is MouseButtonEventArgs) {
-				MouseButtonEventArgs clickEventArgs = (MouseButtonEventArgs)e.StagingItem.Input;
+    private void StartCaptureMouse() { InputManager.Current.PreProcessInput += InputManager_PreProcessInput; }
+    private void StopCaptureMouse() { InputManager.Current.PreProcessInput -= InputManager_PreProcessInput; }
 
-				if (clickEventArgs.ChangedButton == MouseButton.Left &&
-				clickEventArgs.LeftButton == MouseButtonState.Pressed) {
-					if (IsEditing && !EditingTextBox.IsMouseOver) {
-						EndEditing();
-					}
-				}
-			} else if (e.StagingItem.Input is KeyEventArgs) {
-				KeyEventArgs keyEventArgs = (KeyEventArgs)e.StagingItem.Input;
+    private void EventArea_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+        if (e.ChangedButton == MouseButton.Left) {
+            StartEditing();
+            e.Handled = true;
+        }
+    }
 
-				if (keyEventArgs.IsDown && keyEventArgs.Key == Key.Return) {
-					if (IsEditing) {
-						EndEditing();
-					}
-				}
-			}
-		}
+    private void InputManager_PreProcessInput(object sender, PreProcessInputEventArgs e) {
+        if (e.StagingItem.Input is MouseButtonEventArgs) {
+            MouseButtonEventArgs clickEventArgs = (MouseButtonEventArgs)e.StagingItem.Input;
 
-	}
+            if (clickEventArgs.ChangedButton == MouseButton.Left && clickEventArgs.LeftButton == MouseButtonState.Pressed)
+                if (IsEditing && !EditingTextBox.IsMouseOver)
+                    EndEditing();
+        } else if (e.StagingItem.Input is KeyEventArgs) {
+            KeyEventArgs keyEventArgs = (KeyEventArgs)e.StagingItem.Input;
+
+            if (keyEventArgs.IsDown && keyEventArgs.Key == Key.Return)
+                if (IsEditing)
+                    EndEditing();
+        }
+    }
 }
