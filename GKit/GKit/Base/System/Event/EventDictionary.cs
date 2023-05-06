8297a20b68acb6a3dee687cdad30fa685f396f9f
TTOOWA <isg1153@naver.com>
2023-05-06 12:46:35 +0900

    - EventDictionary 추가 - 일부 Utility파일 경로변경

---
+++ GKit/GKit/Base/System/Event/EventDictionary.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+
+namespace GKit;
+
+public class EventDictionary {
+    private Dictionary<string, List<Action>> eventDictionary = new();
+
+    public EventDictionary() { }
+
+    public void Clear() { eventDictionary.Clear(); }
+
+    public void AddListener(string eventName, Action listener) {
+        if (!eventDictionary.ContainsKey(eventName))
+            eventDictionary.Add(eventName, new List<Action>());
+        eventDictionary[eventName].Add(listener);
+    }
+
+    public void RemoveListener(string eventName, Action listener) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        eventDictionary[eventName].Remove(listener);
+
+        if (eventDictionary[eventName].Count == 0)
+            eventDictionary.Remove(eventName);
+    }
+
+    public void ClearListener(string eventName) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        eventDictionary[eventName].Clear();
+        eventDictionary.Remove(eventName);
+    }
+
+    public void Invoke(string eventName) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        foreach (Action listener in eventDictionary[eventName]) { listener?.Invoke(); }
+    }
+}
+
+public class EventDictionary<T> {
+    private Dictionary<string, List<Action<T>>> eventDictionary = new();
+
+    public EventDictionary() { }
+
+    public void Clear() { eventDictionary.Clear(); }
+
+    public void AddListener(string eventName, Action<T> listener) {
+        if (!eventDictionary.ContainsKey(eventName))
+            eventDictionary.Add(eventName, new List<Action<T>>());
+        eventDictionary[eventName].Add(listener);
+    }
+
+    public void RemoveListener(string eventName, Action<T> listener) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        eventDictionary[eventName].Remove(listener);
+
+        if (eventDictionary[eventName].Count == 0)
+            eventDictionary.Remove(eventName);
+    }
+
+    public void ClearListener(string eventName) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        eventDictionary[eventName].Clear();
+        eventDictionary.Remove(eventName);
+    }
+
+    public void Invoke(string eventName, T arg) {
+        if (!eventDictionary.ContainsKey(eventName))
+            return;
+        foreach (Action<T> listener in eventDictionary[eventName]) { listener?.Invoke(arg); }
+    }
+}
