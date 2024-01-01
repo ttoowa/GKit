952812ec5a3b37c09355f0eab87028996f354dac
TTOOWA <isg1153@naver.com>
2024-01-01 17:12:50 +0900

    - SelectedItemSet 에서 First, Last가 null일 때도 반환할 수 있도록 수정

--- GKit/GKit/Base/System/Collections/SelectedItemSet.cs
+++ GKit/GKit/Base/System/Collections/SelectedItemSet.cs
@@ -4,84 +4,84 @@ using System.Collections.Generic;
 using System.Linq;
 
 #if OnUnity
-namespace GKitForUnity
+namespace GKitForUnity;
 #elif OnWPF
-namespace GKitForWPF
+namespace GKitForWPF;
 #else
-namespace GKit
+namespace GKit;
 #endif
-{
-    public class SelectedItemSet : IEnumerable<ISelectable> {
-        public delegate void ItemDelegate(ISelectable item);
-
-        public int Count => itemSet.Count;
-
-        private readonly HashSet<ISelectable> itemSet;
-
-        public ISelectable First => itemSet.First();
-        public ISelectable Last => itemSet.Last();
-
-        public event ItemDelegate SelectionAdded;
-        public event ItemDelegate SelectionRemoved;
 
 
-        public SelectedItemSet() {
-            itemSet = new HashSet<ISelectable>();
+public class SelectedItemSet : IEnumerable<ISelectable> {
+    public delegate void ItemDelegate(ISelectable item);
+
+    public int Count => itemSet.Count;
+
+    private readonly HashSet<ISelectable> itemSet;
+
+    public ISelectable First => itemSet.FirstOrDefault();
+    public ISelectable Last => itemSet.LastOrDefault();
+
+    public event ItemDelegate SelectionAdded;
+    public event ItemDelegate SelectionRemoved;
+
+
+    public SelectedItemSet() {
+        itemSet = new HashSet<ISelectable>();
+    }
+
+    public void Clear() {
+        foreach (ISelectable item in itemSet.ToArray()) {
+            Remove(item);
         }
 
-        public void Clear() {
-            foreach (ISelectable item in itemSet.ToArray()) {
-                Remove(item);
-            }
+        itemSet.Clear();
+    }
 
-            itemSet.Clear();
+    public void Add(ISelectable item) {
+        itemSet.Add(item);
+        item.SetSelected(true);
+
+        SelectionAdded?.Invoke(item);
+    }
+
+    public void Remove(ISelectable item) {
+        itemSet.Remove(item);
+        item.SetSelected(false);
+
+        SelectionRemoved?.Invoke(item);
+    }
+
+    public void SetSingle(ISelectable item) {
+        if (itemSet.Count == 0 && item == null) {
+            return;
         }
 
-        public void Add(ISelectable item) {
-            itemSet.Add(item);
-            item.SetSelected(true);
-
-            SelectionAdded?.Invoke(item);
+        if (itemSet.Count == 1 && itemSet.Contains(item)) {
+            return;
         }
 
-        public void Remove(ISelectable item) {
-            itemSet.Remove(item);
-            item.SetSelected(false);
+        Clear();
+        Add(item);
+    }
 
-            SelectionRemoved?.Invoke(item);
-        }
+    public bool Contains(ISelectable item) {
+        return itemSet.Contains(item);
+    }
 
-        public void SetSingle(ISelectable item) {
-            if (itemSet.Count == 0 && item == null) {
-                return;
-            }
+    public IEnumerable<ISelectable> Where(Func<ISelectable, bool> predicate) {
+        return itemSet.Where(predicate);
+    }
 
-            if (itemSet.Count == 1 && itemSet.Contains(item)) {
-                return;
-            }
+    public IEnumerable<TResult> Select<TResult>(Func<ISelectable, TResult> selector) {
+        return itemSet.Select(selector);
+    }
 
-            Clear();
-            Add(item);
-        }
+    public IEnumerator<ISelectable> GetEnumerator() {
+        return itemSet.GetEnumerator();
+    }
 
-        public bool Contains(ISelectable item) {
-            return itemSet.Contains(item);
-        }
-
-        public IEnumerable<ISelectable> Where(Func<ISelectable, bool> predicate) {
-            return itemSet.Where(predicate);
-        }
-
-        public IEnumerable<TResult> Select<TResult>(Func<ISelectable, TResult> selector) {
-            return itemSet.Select(selector);
-        }
-
-        public IEnumerator<ISelectable> GetEnumerator() {
-            return itemSet.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator() {
-            return itemSet.GetEnumerator();
-        }
+    IEnumerator IEnumerable.GetEnumerator() {
+        return itemSet.GetEnumerator();
     }
 }
